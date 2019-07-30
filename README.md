d13498069a4706807378212ec7e419b107250ea7
beulathamban <35990861+beulathamban@users.noreply.github.com>
2019-07-30 16:41:52 +1200

    added hamburger

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Nav
-Responsive Navigation
+# nav
---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log('hi');
+
+// $('nav').hide();
+
+// click even for hamburger
+$('.myicon').click(function(){
+  $('nav').toggle();
+});
+
